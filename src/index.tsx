c48f28184346f6a365deb6d7fc6eaaeadf8413e7
EggTronic <danliangchen@gmail.com>
2020-03-05 17:18:42 +0800

    [update] font

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import Radio from "./Radio/radio";
 import RadioGroup from "./Radio/radioGroup";
 
 import "nes.css/css/nes.min.css";
+import "./assets/fonts/font.scss";
 import "./index.scss";
 
 export { Button, Section, Text, Radio, RadioGroup };
